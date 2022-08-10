435e7487d2e4bf921f31af6add7f7c83a2b41470
Jacson <jacson.analistaderede@gmail.com>
2022-08-10 03:32:32 -0400

    Atualização de codigo

--- app/app.js
+++ app/app.js
@@ -1,1 +1,25 @@
 console.log('Ola estou começando a programar em JS')
+
+var nome = 'Jacson Silva';
+var idade = 41;
+
+console.log(nome, '-->nome');
+console.log(idade, '-->idade');
+
+a = 4;
+b =10;
+console.log(a*b);
+
+/* EStruturas de Decisão*/
+var mediajhon = 4;
+if (mediajhon >=7){
+    console.log("Parabéns Jhon , você foi aprovado!!")
+    
+
+}else if(mediajhon >=5){
+    console.log("Voce esta de Recuperação")
+}else{
+    console.log("Desculpa Jhon, você foi reprovado !")
+}
+
+/*Matrizes*/
