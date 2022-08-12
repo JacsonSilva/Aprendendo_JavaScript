1199d39278f6692c2def25b74e1679fcc4f7e83b
Jacson <jacson.analistaderede@gmail.com>
2022-08-11 20:58:01 -0400

    Adcionando exemplos de laço de repetições em JS

--- app/app.js
+++ app/app.js
@@ -22,4 +22,84 @@ if (mediajhon >=7){
     console.log("Desculpa Jhon, você foi reprovado !")
 }
 
-/*Matrizes*/
+/*Arrays em Java Script*/
+ const produto=("arroz,açucar,café,feijao")
+ console.log(produto)
+
+ /* outro exemplo de Arrays*/
+
+ const carro=["ford", "KA", 2017, 85000]
+ console.log(carro)
+
+/*Loops e laço de Repetição*/
+/*emxemplo
+const list=["Agua". "Leite","Ovos", "refrigerante","Cerveja"];
+console list(lista[0]);
+console list(lista[1]);
+console list(lista[2]);
+console list(lista[3]);
+console list(lista[4]);
+console list(lista[5]);
+
+//for (Declaração; condição;interação)
+
+for(let indice =0; indice <lista.length; indice = indice +1){
+
+    console.log(lista[indice])
+}
+
+//for..in ==>  Interegae com chaves
+for(let item in listaCompras){
+    console.log(item)
+}
+
+//for..of
+for (let item of listCompras){
+    console.log("for...of" , item)
+}
+
+//while ==> Significa enquanto
+
+while (horas<=12){
+    console.log("Bom dia");
+    horas = horas +1;
+
+}
+
+//do..while
+  let horas =13;
+    do{
+        console.log("Bom dia!");
+        horas = horas +1;
+
+} while (horas <= 12);
+
+//forEach
+    const usuarios =[
+        {
+            usename = "Jacson";
+            age=41
+        }
+
+        {
+            usename = "Debora";
+            Age=35
+        }
+        {
+            usename = "Ruth";
+            age = 18
+        }
+        {
+            usename = "Elias";
+            age=22
+        }
+    
+    ]
+
+    usuario.forEach(fucntion(usuario){
+        console.log(user.username);
+        console.log(user.age)
+    }
+
+
+*/
